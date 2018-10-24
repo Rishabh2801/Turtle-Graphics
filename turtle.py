7b0a38776c737372ad6b0e36aebd2b0c0ebd139f
Rishabh <34177647+Rishabh2801@users.noreply.github.com>
2018-10-24 11:52:58 +0530

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,14 +1,14 @@
-import turtle
+import turtle  
 x=0
 turtle.speed(0)
 turtle.bgcolor('black')
-while x<500:
-   if x%2==0:
+while x<500:     # This while loop is for stop making spirals so when x reaches 500 it stops making more spirals
+   if x%2==0:    # If x is even or divisible by 2 then the color will be red
          turtle.pencolor('red')
    elif x%3==0:
-         turtle.pencolor('blue')
+         turtle.pencolor('blue')  # for making visually appealing i made another color which will be activated when it is divisible by 3
    else:
-         turtle.pencolor('green')
+         turtle.pencolor('green')  # if above 2 condition is false then it takes green color so with three colors it looks fantastic
    turtle.forward(x)
    turtle.left(90.91)
    x=x+1
